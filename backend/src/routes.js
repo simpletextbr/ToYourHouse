e9d36188a49dc40bfbd33a331fdb9b26da46a69d
simpletextbr <simpletextbr@gmail.com>
2020-04-30 17:51:59 -0300

    feat: configurando primeira rota e conexao com o banco

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,8 +1,28 @@
 const  express = require('express');
 
+const connection = require('./database/connection');
+
 const routes = express.Router();
 
+routes.post('/singin', async(req, res) => {
+    const { name, phone, address, city, uf} = req.body;
+    let { password } = req.body;
+    // hash simples de seguranca 
+    let hash = 5;
+    hash = 59 * password + hash;
+    password=hash
 
+    await connection('enterprise').insert({
+        name,
+        phone,
+        address,
+        city,
+        uf,
+        password
+    });
+
+    return res.json({send:'sucessfull'});
+})
 
 
 
