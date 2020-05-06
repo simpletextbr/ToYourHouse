20c7981d9d380e165e946eb147d830df6bbd7e90
simpletextbr <simpletextbr@gmail.com>
2020-05-06 18:12:05 -0300

    Feat: paginacao

--- backend/src/controllers/MobileController.js
+++ backend/src/controllers/MobileController.js
@@ -2,8 +2,17 @@ const connection = require('../database/connection');
 
 module.exports = {
 async index(req, res){
-    const list = await connection('enterprise').select('*');
+    const { page = 1 } = req.query;
+    const [count] = await connection('enterprise')
+    .count();
 
+    const list = await connection('enterprise')
+    .limit(8)
+    .offset((page - 1 ) * 8)
+    .select('*');
+
+
+    res.header('Amazing-Enterprises', count['count(*)']);
     res.json(list);
     }
 }
