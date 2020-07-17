db25dc2373a98b1c426d91163a4369515ef64472
simpletextbr <simpletextbr@gmail.com>
2020-07-17 16:12:18 -0300

    Feat: Adicionado opcional de Adds

--- backend/src/controllers/ProductsController.js
+++ backend/src/controllers/ProductsController.js
@@ -1,61 +1,59 @@
-const connection = require('../database/connection');
+const connection = require("../database/connection");
 
-module.exports ={
-    async Index_Products(req, res){
-        const enterprise_id = req.headers.authorization;
-        const list = await connection('products')
-        .where('enterprise_id', enterprise_id)
-        .select('*');
+module.exports = {
+  async Index_Products(req, res) {
+    const enterprise_id = req.headers.authorization;
+    const list = await connection("products")
+      .where("enterprise_id", enterprise_id)
+      .select("*");
 
-        
-
-        if(!list[0]){
-            return res.json(null)
-         }
-
-        return res.json(list);
-
-    },
-    async create_Products(req, res){
-        const enterprise_id = req.headers.authorization;
-        const cat_id = req.headers.cat_authorization;
-        const verifyId = await connection('enterprise').select('id').where({id:enterprise_id});
-        const verityCat = await connection('categoryProducts').select('id').where({id:cat_id});
-
-        if(!verifyId[0] || !verityCat[0]){
-            return res.status(401).json({ error:'Not Authorized'});
-        }else{
-
-            const{ name, Ing, price } = req.body;
-
-            await connection('products').insert({
-                name,
-                Ing,
-                price,
-                enterprise_id,
-                cat_id
-            })
-
-        }
-        return res.json({send:'sucessfull'});
-    },
-    async delete_Products(req, res){
-        const { id } = req.params;
-        const enterprise_id = req.headers.authorization;
-
-        const verify = await connection('products')
-        .where('id', id)
-        .select('enterprise_id')
-        .first();
-
-        if(verify.enterprise_id != enterprise_id){
-            return res.status(401).json({error:'Not Authorized'});
-        }else{
-
-        await connection('products').where('id', id).delete();
-
-        return res.status(204).send();
-        }
+    if (!list[0]) {
+      return res.json(null);
     }
 
-}
+    return res.json(list);
+  },
+  async create_Products(req, res) {
+    const enterprise_id = req.headers.authorization;
+    const cat_id = req.headers.cat_authorization;
+    const verifyId = await connection("enterprise")
+      .select("id")
+      .where({ id: enterprise_id });
+    const verityCat = await connection("categoryProducts")
+      .select("id")
+      .where({ id: cat_id });
+
+    if (!verifyId[0] || !verityCat[0]) {
+      return res.status(401).json({ error: "Not Authorized" });
+    } else {
+      const { name, Ing, price, adds } = req.body;
+
+      await connection("products").insert({
+        name,
+        Ing,
+        price,
+        adds,
+        enterprise_id,
+        cat_id,
+      });
+    }
+    return res.json({ send: "sucessfull" });
+  },
+  async delete_Products(req, res) {
+    const { id } = req.params;
+    const enterprise_id = req.headers.authorization;
+
+    const verify = await connection("products")
+      .where("id", id)
+      .select("enterprise_id")
+      .first();
+
+    if (verify.enterprise_id != enterprise_id) {
+      return res.status(401).json({ error: "Not Authorized" });
+    } else {
+      await connection("products").where("id", id).delete();
+
+      return res.status(204).send();
+    }
+  },
+};
--- frontend/src/pages/Inicio/newProduct/index.js
+++ frontend/src/pages/Inicio/newProduct/index.js
@@ -1,126 +1,142 @@
-import React, { useEffect , useState} from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { Link, useHistory } from "react-router-dom";
 
-import api from '../../../services/api';
+import api from "../../../services/api";
 
-import Header from '../../Utils/Header'
-import { FiArrowLeft} from 'react-icons/fi';
-import './styles.css';
+import Header from "../../Utils/Header";
+import { FiArrowLeft } from "react-icons/fi";
+import "./styles.css";
 
-export default function Newproduct(){
-//Categorias
-const[category, setCategory] = useState([]);
-//Novo Produto
-const[proName, setProname] = useState('');
-const[proIng, setProing] = useState('');
-const[proPrice, setProprice] = useState('');
-const[cat_id, setCat_id] = useState('');
+export default function Newproduct() {
+  //Categorias
+  const [category, setCategory] = useState([]);
+  //Novo Produto
+  const [proName, setProname] = useState("");
+  const [proIng, setProing] = useState("");
+  const [proPrice, setProprice] = useState("");
+  const [cat_id, setCat_id] = useState("");
+  const [adds, setAdds] = useState();
 
-const enterprise_id = localStorage.getItem('enterpriseid');
-const history = useHistory();
+  const enterprise_id = localStorage.getItem("enterpriseid");
+  const history = useHistory();
 
-async function createProduct(e){
-    e.preventDefault()
+  async function createProduct(e) {
+    e.preventDefault();
 
-    const name=proName;
-    const Ing=proIng;
-    const price= proPrice;
+    const name = proName;
+    const Ing = proIng;
+    const price = proPrice;
 
     const data = {
-        name,
-        Ing,
-        price
-    }
-    if(cat_id==="Selecione uma categoria" || cat_id.trim()===""){
-        alert('Você precisa selecionar uma categoria!');
+      name,
+      Ing,
+      price,
+      adds,
+    };
 
-    }else{
-        try{
-        await api.post('/products', data,{
-            headers:{
-                Authorization: enterprise_id,
-                cat_Authorization: cat_id
-            }
-        });
-        history.push('/inicio')
-        }catch(error){
-        alert('Não foi possivel criar seu produto, tente novamente');
-    }
-
-    } 
-}
-
-//List Category
-useEffect(() => {
-    api.get('/category',{
-        headers:{
+    if (cat_id === "Selecione uma categoria" || cat_id.trim() === "") {
+      alert("Você precisa selecionar uma categoria!");
+    } else {
+      try {
+        await api.post("/products", data, {
+          headers: {
             Authorization: enterprise_id,
-        }
-    }).then(response => {
+            cat_Authorization: cat_id,
+          },
+        });
+        history.push("/inicio");
+      } catch (error) {
+        alert("Não foi possivel criar seu produto, tente novamente");
+      }
+    }
+  }
+
+  //List Category
+  useEffect(() => {
+    api
+      .get("/category", {
+        headers: {
+          Authorization: enterprise_id,
+        },
+      })
+      .then((response) => {
         setCategory(response.data);
-    });
-    }, [enterprise_id]);
+      });
+  }, [enterprise_id]);
 
-
-
-
-
-
-
-
-
-return (
-<>
-<Header />
-<main>
-    <div className="add-product">
-        <p className="title">Opa! Produto novo chegando!!</p>
-        <form onSubmit={createProduct}>
+  return (
+    <>
+      <Header />
+      <main>
+        <div className="add-product">
+          <p className="title">Opa! Produto novo chegando!!</p>
+          <form onSubmit={createProduct}>
             <p>Nome do Produto</p>
             <input
-            placeholder="Seu novo produto"
-            required
-            value={proName}
-            onChange={e => setProname(e.target.value)}
+              placeholder="Seu novo produto"
+              required
+              value={proName}
+              onChange={(e) => setProname(e.target.value)}
             />
             <p>Ingredientes Basicos</p>
             <textarea
-            placeholder="Ingredientes Principais do seu Produto"
-            value={proIng}
-            onChange={e => setProing(e.target.value)}
+              placeholder="Ingredientes Principais do seu Produto"
+              value={proIng}
+              onChange={(e) => setProing(e.target.value)}
             />
             <p>Preço</p>
             <input
-            type="number"
-            required
-            placeholder="R$0,00"
-            value={proPrice}
-            onChange={e => setProprice(e.target.value)}
+              type="number"
+              required
+              placeholder="R$0,00"
+              value={proPrice}
+              onChange={(e) => setProprice(e.target.value)}
             />
             <p>Categoria</p>
-            <select 
-            value={cat_id} 
-            onChange={e => setCat_id(e.target.value)}
-            >
-                    <option value="-1">Selecione uma categoria</option>
-                    {category.map(categorys =>
-                        <option key={categorys.id} value={categorys.id}>
-                            {categorys.name}
-                        </option>
-                        )}
+            <select value={cat_id} onChange={(e) => setCat_id(e.target.value)}>
+              <option value="-1">Selecione uma categoria</option>
+              {category.map((categorys) => (
+                <option key={categorys.id} value={categorys.id}>
+                  {categorys.name}
+                </option>
+              ))}
             </select>
-            <div className='row-buttons'>
-            <Link className="Back_Interations" to="/inicio">
-                <FiArrowLeft size={16} color='#FF5555' />
-                    Voltar
-            </Link>
-            <button className="btn" type="submit">CADASTRAR</button>
+            <div className="row-checkbox">
+              <p className="title-checkbox">
+                Esse Produdo pode receber acréscimos ?
+              </p>
+              <input
+                className="Sim"
+                type="checkbox"
+                name="Sim"
+                value={true}
+                onChange={(e) => setAdds(e.target.value)}
+              />{" "}
+              <i>Sim</i>
+              <input
+                className="Nao"
+                type="checkbox"
+                name="Nao"
+                value={false}
+                onChange={(e) => setAdds(e.target.value)}
+              />{" "}
+              <i>Não</i>
             </div>
-        </form>
-    </div>
-</main>
-<footer><p>2020@ Todos Os Direitos Reservatos. Developed by PlanUnity Inc.</p></footer>
-</>
-)
+            <div className="row-buttons">
+              <Link className="Back_Interations" to="/inicio">
+                <FiArrowLeft size={16} color="#FF5555" />
+                Voltar
+              </Link>
+              <button className="btn" type="submit">
+                CADASTRAR
+              </button>
+            </div>
+          </form>
+        </div>
+      </main>
+      <footer>
+        <p>2020@ Todos Os Direitos Reservatos. Developed by PlanUnity Inc.</p>
+      </footer>
+    </>
+  );
 }
-                    
